ca80c233b22bb4a2a5a6b8a992bdcafc4c8b42a2
Andrei Condurachi <andrei@condurachi.ro>
2020-11-21 22:26:27 +0200

    updates

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+# syslog option '-Z' was changed to '-t', change this in /etc/conf.d/syslog so that syslog (and then cron) actually starts
+# https://gitlab.alpinelinux.org/alpine/aports/-/issues/9279
+exec sed -i 's/SYSLOGD_OPTS="-Z"/SYSLOGD_OPTS="-t"/g' /etc/conf.d/syslog
+
 # Create cron job for Grav maintenance scripts
 (crontab -l; echo "* * * * * cd /var/www/html;/usr/bin/php bin/grav scheduler 1>> /dev/null 2>&1") | crontab -
 # Cron requires that each entry in a crontab end in a newline character. If the last entry in a crontab is missing the newline, cron will consider the crontab (at least partially) broken and refuse to install it.
 (crontab -l; echo "") | crontab -
+
+# syslog option '-Z' was changed to '-t', change this in /etc/conf.d/syslog so that syslog (and then cron) actually starts
+# https://gitlab.alpinelinux.org/alpine/aports/-/issues/9279
+RUN sed -i 's/SYSLOGD_OPTS="-Z"/SYSLOGD_OPTS="-t"/g' /etc/conf.d/syslog
 
 # Start Apache, PHP-FPM and Cron
 exec /usr/sbin/httpd -D FOREGROUND -f /etc/apache2/httpd.conf &
