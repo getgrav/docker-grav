928f671be7fe0183a8b9b5f0f447fcb1e50a62f4
Andrei Condurachi <andrei@condurachi.ro>
2020-11-21 17:49:08 +0200

    Cron updates

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Create cron job for Grav maintenance scripts
-exec (crontab -l; echo "* * * * * cd /var/www/html;/usr/local/bin/php bin/grav scheduler 1>> /dev/null 2>&1") | crontab -
+(crontab -l; echo "* * * * * cd /var/www/html;/usr/bin/php bin/grav scheduler 1>> /dev/null 2>&1") | crontab -
+# Cron requires that each entry in a crontab end in a newline character. If the last entry in a crontab is missing the newline, cron will consider the crontab (at least partially) broken and refuse to install it.
+(crontab -l; echo "") | crontab -
 
 # Start PHP-FPM and Apache
 exec /usr/sbin/httpd -D FOREGROUND -f /etc/apache2/httpd.conf &
